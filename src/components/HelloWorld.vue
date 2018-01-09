f2b55c92c82a9b7f3dd237e260824af9158d99ca
Dzinlife <dzinlife@me.com>
2018-01-09 17:01:15 +0800

    save

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <template>
-  <canvas ref="canvas" :style="{width: '200px', height: '200px'}" @mousemove="mouseMoveHandler"/>
+  <canvas ref="canvas" :style="{width: '100%', height: '500px'}" @mousemove="mouseMoveHandler"/>
 </template>
 
 <script>
-import webgl from '@/services/webgl'
+import {initShaders} from '@/utils'
+import VSHADER_SOURCE from '@/services/VShaderSource.vs'
+import FSHADER_SOURCE from '@/services/FShaderSource.fs'
 import Matrix4 from '@/utils/matrix'
 
 export default {
@@ -14,13 +16,45 @@ export default {
     canvas.width = canvas.clientWidth * devicePixelRatio
     canvas.height = canvas.clientHeight * devicePixelRatio
 
-    const gl = webgl(canvas)
+    const gl = canvas.getContext('webgl')
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+      alert('Failed to get the rendering context for WebGL')
+      return
+    }
     this.gl = gl
+
+    window.addEventListener('optimizedResize', this.resize)
+
+    this.aPosition = gl.getAttribLocation(gl.program, 'a_Position')
+    this.aPointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+    this.aColor = gl.getAttribLocation(gl.program, 'a_Color')
+    this.uFragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
+    this.uXformMatrix = gl.getUniformLocation(gl.program, 'u_xfromMatrix')
+    this.uWidth = gl.getUniformLocation(gl.program, 'u_Width')
+    this.uHeight = gl.getUniformLocation(gl.program, 'u_Height')
+
+    gl.uniform1f(this.uWidth, gl.drawingBufferWidth)
+    gl.uniform1f(this.uHeight, gl.drawingBufferHeight)
+
+    this.initVertexBuffer()
 
     requestAnimationFrame(this.tick)
   },
 
+  beforeMount() {
+    window.removeEventListener('optimizedResize', this.resize)
+  },
+
   methods: {
+    resize() {
+      const canvas = this.$refs.canvas
+      const {gl} = this
+      const devicePixelRatio = window.devicePixelRatio || 1
+      canvas.width = canvas.clientWidth * devicePixelRatio
+      canvas.height = canvas.clientHeight * devicePixelRatio
+      gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+    },
+
     tick() {
       this.draw()
       requestAnimationFrame(this.tick)
@@ -32,42 +66,43 @@ export default {
       gl.clear(gl.COLOR_BUFFER_BIT)
     },
 
+    initVertexBuffer() {
+      const {gl} = this
+
+      this.vertices = new Float32Array(15)
+      const FSIZE = this.vertices.BYTES_PER_ELEMENT
+
+      const vertexBuffer = gl.createBuffer()
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+      gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW)
+      gl.vertexAttribPointer(this.aPosition, 2, gl.FLOAT, false, FSIZE * 5, 0)
+      gl.vertexAttribPointer(this.aColor, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2)
+      gl.enableVertexAttribArray(this.aPosition)
+      gl.enableVertexAttribArray(this.aColor)
+
+      const xformMatrix = new Matrix4()
+      xformMatrix.setRotate(0, 0, 0, 1)
+      xformMatrix.scale(1.5, 1, 1)
+      gl.uniformMatrix4fv(this.uXformMatrix, false, xformMatrix.elements)
+    },
+
     draw() {
       const {gl} = this
       this.clear(gl)
 
-      const aPosition = gl.getAttribLocation(gl.program, 'a_Position')
-      const aPointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
-      const aColor = gl.getAttribLocation(gl.program, 'a_Color')
-      const uFragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
-      const uXformMatrix = gl.getUniformLocation(gl.program, 'u_xfromMatrix')
-
       const {x, y} = this
       const t = Date.now() / 100
-      const vertices = new Float32Array([
+      this.vertices.set([
         0.0, 0.0, 1.0, 1.0, 1.0,
         x - 0.3 * Math.sin(t), y - 0.3 * Math.cos(t), 1.0, 0.0, 1.0,
         x + 0.3 * Math.sin(t), y + 0.3 * Math.cos(t), 0.0, 1.0, 1.0
       ])
-      const FSIZE = vertices.BYTES_PER_ELEMENT
-
-      const vertexBuffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-      gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, FSIZE * 5, 0)
-      gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2)
-      gl.enableVertexAttribArray(aPosition)
-      gl.enableVertexAttribArray(aColor)
+      gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.DYNAMIC_DRAW)
 
       // gl.vertexAttrib3f(aPosition, this.x, this.y, 0.0)
-      gl.vertexAttrib1f(aPointSize, 20.0)
-      // gl.uniform4f(uFragColor, Math.random(), Math.random(), Math.random(), Math.random())
-      gl.uniform4f(uFragColor, 0.0, 0.0, 0.0, 1.0)
-      const xformMatrix = new Matrix4()
-      xformMatrix.setRotate(0, 0, 0, 1)
-      xformMatrix.scale(1.5, 1, 1)
-      // console.log(xformMatrix.elements)
-      gl.uniformMatrix4fv(uXformMatrix, false, xformMatrix.elements)
+      // gl.vertexAttrib1f(this.aPointSize, 20.0)
+      // gl.uniform4f(this.uFragColor, Math.random(), Math.random(), Math.random(), Math.random())
+      // gl.uniform4f(this.uFragColor, 0.0, 0.0, 0.0, 1.0)
 
       gl.drawArrays(gl.TRIANGLES, 0, 3)
     },
--- src/services/FShaderSource.fs
+++ src/services/FShaderSource.fs
@@ -1,7 +1,9 @@
 precision mediump float;
-uniform vec4 u_FragColor;
-varying vec4 v_Color;
+uniform float u_Width;
+uniform float u_Height;
+// uniform vec4 u_FragColor;
+// varying vec4 v_Color;
 
 void main() {
-  gl_FragColor = v_Color;
+  gl_FragColor = vec4(gl_FragCoord.x / u_Width, gl_FragCoord.y / u_Height, 0.0, 1.0);
 }
